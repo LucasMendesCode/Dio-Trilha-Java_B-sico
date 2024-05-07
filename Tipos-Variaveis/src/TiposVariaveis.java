618b13b3d9fe1e09525e71d1ec467a10cd1065b3
Lucas <lucasmendescode@gmail.com>
2024-05-06 23:38:42 -0300

    alteração 2 com notas

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -6,5 +6,11 @@ public class tiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short nuemroCurto2 =  (short) numeroNormal;
+
+        final double FINAL_PI_VALUE = 3.14; 
+        //caixa alta se torna uma constante
+
+         String meuNome = "meu nome é lucas"; 
+         //string se refere a classe de testo e verificar os tipos primitivos
     }
 }
