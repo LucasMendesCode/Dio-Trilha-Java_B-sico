240e23f40e93b324bc52dccdc2b62f98796e6d49
Lucas <lucasmendescode@gmail.com>
2024-05-06 23:32:06 -0300

    alteração

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
-public class App {
+public class tiposVariaveis {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short nuemroCurto2 =  (short) numeroNormal;
     }
 }
