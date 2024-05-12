a4931517d3a8f0dc8dd4317d32a9c352365e95d2
Lucas <lucasmendescode@gmail.com>
2024-05-12 10:46:56 -0300

    com abreviação ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,16 +1,16 @@
   //GERADOR DE NOTAS ALEATORIAS.
   //implementação de notas aleatorias.
+  //estrutura condicional composta.
 import java.util.Random;
 
 public class ResultadoEscolar {
-    //estrutura condicional composta.
 
     public static void main(String[] args) {
         Random rand = new Random();
         int nota = rand.nextInt (11);
         // essa foi substituida pela nota de cima "int nota = 6;"
-        
-         if (nota >= 7) {
+
+        if (nota >= 7) {
         System.out.println("Aprovado! Nota: " + nota);
          }
         
@@ -21,5 +21,10 @@ public class ResultadoEscolar {
         else {
         System.out.println("Reprovado. Nota: " + nota); 
         }
+
+     // ABREVIAÇÃO TERNARIA: if, else if, else :
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7? "Recuperação" : "reporvado";
+  System.out.println(resultado + ": " + nota);
     }
 }
